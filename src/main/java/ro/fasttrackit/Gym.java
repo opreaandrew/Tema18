378d81d6f4aef7ae40cce72dcd1b1c6c3e94dd3a
Andrei Oprea <opreaandrew@yahoo.com>
2023-11-11 15:54:06 +0200

    Wrote most of it, tested nothing

--- src/main/java/ro/fasttrackit/Gym.java
+++ src/main/java/ro/fasttrackit/Gym.java
@@ -1,5 +1,9 @@
 package ro.fasttrackit;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+import java.nio.file.Path;
 import java.time.Duration;
 import java.time.LocalDateTime;
 import java.util.*;
@@ -20,7 +24,7 @@ public class Gym {
         return subscriptions;
     }
 
-    public Double averageAgeOfMembers(){
+    public Double averageAgeOfMembers() {
         int presentYear = LocalDateTime.now().getYear();
         return members.stream()
                 .map(GymMember::birthdate)
@@ -29,27 +33,27 @@ public class Gym {
                 .orElseThrow(() -> new IllegalArgumentException("No members to calculate for."));
     }
 
-    public Optional<GymMember> maximumAgeOfMembers(){
+    public Optional<GymMember> maximumAgeOfMembers() {
         int presentYear = LocalDateTime.now().getYear();
         return members.stream()
                 .max((m1, m2) -> m1.birthdate().compareTo(m2.birthdate()));
     }
 
-    public Optional<GymMember> minimumAgeOfMembers(){
+    public Optional<GymMember> minimumAgeOfMembers() {
         int presentYear = LocalDateTime.now().getYear();
         return members.stream()
                 .min((m1, m2) -> m1.birthdate().compareTo(m2.birthdate()));
     }
 
-    public long totalRemainingTime(){
+    public long totalRemainingTime() {
         long time = 0;
-        for (Duration value : subscriptions.values()){
+        for (Duration value : subscriptions.values()) {
             time = value.toHours();
         }
         return time;
     }
 
-    public String informationAboutMember(String memberName){
+    public String informationAboutMember(String memberName) {
         GymMember member = findMemberByName(memberName);
         long remainingTime = subscriptions.get(member).toHours();
         return "%s born on %s, has %s hours left on his/her subscription".formatted(member.name(), member.birthdate(), remainingTime);
@@ -60,6 +64,30 @@ public class Gym {
                 .filter(user -> user.name().equalsIgnoreCase(memberName))
                 .findFirst()
                 .orElseThrow(() -> new IllegalArgumentException("Invalid name or no user named: " + memberName));
+    }
+
+    private void generateReport() {
+        List<String> red = new ArrayList<>();
+        List<String> yellow = new ArrayList<>();
+        List<String> green = new ArrayList<>();
+        subscriptions.forEach((k, v) -> {
+            if (v.toHours() < 10) {
+                red.add(k.name());
+            } else if (v.toHours() < 30) {
+                yellow.add(k.name());
+            } else
+                green.add(k.name());
+        });
+
+        try (Writer writer = new FileWriter(String.valueOf(Path.of("src/main/resources/remaining-time-report-%s".formatted(String.valueOf(LocalDateTime.now())))))) {
+            writer.write("RED: %s".formatted(red));
+            writer.write("\n");
+            writer.write("YELLOW: %s".formatted(yellow));
+            writer.write("\n");
+            writer.write("GREEN: %s".formatted(green));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 }
